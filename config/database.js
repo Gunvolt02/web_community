55fcc5496034e569f720ecba61d669f9eb522a73
Lorenzo Maiuri <maiurilorenzo@gmail.com>
2021-05-26 16:59:37 +0200

    Modifica per DB

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@
 const crypto = require('crypto').randomBytes(256).toString('hex');
 
 module.exports = { // per esportare il file quando richiesto
-  uri: 'mongodb://localhost:27017/web_community',
+  uri: 'mongodb+srv://Lorenzo:Bowser2002@meanapp.y53of.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
   secret: crypto, // codice interno segreto utilizzato per varie autenticazioni (db, token, ecc..)
   db: 'web_community'
 }
